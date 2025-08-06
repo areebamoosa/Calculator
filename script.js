ebf74dfc95463e12de8c8af090b0d819f26e1f10
Areeba Moosa <areebamoosa153@gmail.com>
2025-08-07 00:44:22 +0500

    Basic Calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+let string = "";
+let button = document.querySelectorAll(".btn");
+
+Array.from(button).forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector(".input").value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      document.querySelector(".input").value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.slice(0, -1);
+      document.querySelector(".input").value = string;
+    } else {
+      console.log(e.target);
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
